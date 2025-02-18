33f79b7231c5b2d734720b95ddc544a855b08e0d
Sebastian Bruma <sebastianbruma24@gmail.com>
2025-02-18 10:55:50 +0200

    Add unit events

--- Assets/Scripts/Monobehaviours/UnitSelectionManager.cs
+++ Assets/Scripts/Monobehaviours/UnitSelectionManager.cs
@@ -38,9 +38,13 @@ public class UnitSelectionManager : MonoBehaviour
                 .Build(entityManager);
 
             NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            NativeArray<Selected> selectedArray = entityQuery.ToComponentDataArray<Selected>(Allocator.Temp);
 
             for (int i = 0; i < entityArray.Length; i++){
                 entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+                Selected selected = selectedArray[i];
+                selected.onDeselected = true;
+                entityManager.SetComponentData(entityArray[i], selected);
             }
 
             Rect selectionAreaRect = GetSelectionAreaRect();
@@ -65,6 +69,9 @@ public class UnitSelectionManager : MonoBehaviour
                     if (selectionAreaRect.Contains(unitScreenPosition))
                     {
                         entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                        Selected selected = entityManager.GetComponentData<Selected>(entityArray[i]);
+                        selected.onSelected = true;
+                        entityManager.SetComponentData(entityArray[i], selected);
                     }
                 }
 
@@ -95,6 +102,9 @@ public class UnitSelectionManager : MonoBehaviour
                     if (entityManager.HasComponent<Unit>(raycastHit.Entity))
                     {
                         entityManager.SetComponentEnabled<Selected>(raycastHit.Entity, true);
+                        Selected selected = entityManager.GetComponentData<Selected>(raycastHit.Entity);
+                        selected.onSelected = true;
+                        entityManager.SetComponentData(raycastHit.Entity, selected);
                     }
                 }
             }
--- Assets/Scripts/Systems/SelectedVisualSystem.cs
+++ Assets/Scripts/Systems/SelectedVisualSystem.cs
@@ -1,22 +1,30 @@
+
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 
+[UpdateInGroup(typeof(LateSimulationSystemGroup))]
+[UpdateBefore(typeof(ResetEventSystem))]
 partial struct SelectedVisualSystem : ISystem
 {
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var selected in SystemAPI.Query<RefRO<Selected>>().WithDisabled<Selected>())
+        foreach (var selected in SystemAPI.Query<RefRO<Selected>>().WithPresent<Selected>())
         {
-            var visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
-            visualLocalTransform.ValueRW.Scale = 0f;
-        }
+            if (selected.ValueRO.onSelected)
+            {
+                UnityEngine.Debug.Log("selected");
+                var visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
+                visualLocalTransform.ValueRW.Scale = selected.ValueRO.showScale;
+            }
 
-        foreach (var selected in SystemAPI.Query<RefRO<Selected>>())
-        {
-            var visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
-            visualLocalTransform.ValueRW.Scale = selected.ValueRO.showScale;
+            if (selected.ValueRO.onDeselected)
+            {
+                UnityEngine.Debug.Log("deselected");
+                var visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
+                visualLocalTransform.ValueRW.Scale = 0f;
+            }
         }
     }
 }
